16e08b7a345e8502a16422c9e1572058baa2cc0c
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-01-22 13:22:35 +0200

    finish

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css';
+
+export default function FriendListItem ({ avatar, name, isOnline, id }) {
+    return (<li className={css.item} key={id}>
+        <span className={(isOnline ? css.online : css.offline)}></span>
+        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={css.name}>{name}</p>
+    </li>);
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
